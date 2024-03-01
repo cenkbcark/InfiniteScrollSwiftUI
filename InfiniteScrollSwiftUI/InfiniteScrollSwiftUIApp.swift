6b66000baacb73b9c4ca2baf9232068b1be73380
cenkbcark <cenkbahadircark@gmail.com>
2024-03-01 23:13:56 +0300

    Initial Commit

---
+++ InfiniteScrollSwiftUI/InfiniteScrollSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteScrollSwiftUIApp.swift
+//  InfiniteScrollSwiftUI
+//
+//  Created by Cenk Bahadır Çark on 1.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteScrollSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
