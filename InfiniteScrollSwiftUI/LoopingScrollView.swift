194de94706ff62f187a0379ee90d585245e9b218
cenkbcark <cenkbahadircark@gmail.com>
2024-03-02 00:09:27 +0300

    reversible scroll view

---
+++ InfiniteScrollSwiftUI/LoopingScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  LoopingScrollView.swift
+//  InfiniteScrollSwiftUI
+//
+//  Created by Cenk Bahadır Çark on 1.03.2024.
+//
+
+import SwiftUI
+
+struct LoopingScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoopingScrollView()
+}
---
+++ InfiniteScrollSwiftUI/Model/Item.swift
@@ -0,0 +1,8 @@
+//
+//  Item.swift
+//  InfiniteScrollSwiftUI
+//
+//  Created by Cenk Bahadır Çark on 1.03.2024.
+//
+
+import Foundation
